97d9f8d8a98bf43751e4bc9d5b2de974ab0b4a90
PorterNilsson <porter.nilsson@gmail.com>
2025-10-13 13:43:56 -0600

    Added GET /api/user endpoint complete with pagination

---
+++ src/routes/userRouter.test.js
@@ -0,0 +1,49 @@
+const request = require("supertest");
+const app = require("../service");
+const { Role, DB } = require("../database/database.js");
+
+const testUser = { name: randomName(), email: randomEmail(), password: "a" };
+let testUserAuthToken;
+let adminUserAuthToken;
+
+beforeAll(async () => {
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  testUser.id = registerRes.body.user.id;
+
+  const adminUser = await createAdminUser();
+  const loginRes = await request(app)
+    .put("/api/auth")
+    .send({ email: adminUser.email, password: adminUser.password });
+  expect(loginRes.status).toBe(200);
+  adminUserAuthToken = loginRes.body.token;
+});
+
+test("list users unauthorized", async () => {
+  const listUsersRes = await request(app).get("/api/user");
+  expect(listUsersRes.status).toBe(401);
+});
+
+test("list users", async () => {
+  const listUsersRes = await request(app)
+    .get("/api/user")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`);
+  expect(listUsersRes.status).toBe(200);
+});
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+function randomEmail() {
+  return `${randomName()}@test.com`;
+}
+
+async function createAdminUser() {
+  let user = { password: "admin", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = `${user.name}@admin.com`;
+
+  user = await DB.addUser(user);
+  return { ...user, password: "admin" };
+}
